c6702920891497f2ce0c19aa1e188a419125b7a3
Mazzlax <mazzlaxaani@gmail.com>
2024-06-28 16:50:13 +0300

    initial commit
    new projuct upload github

--- lib/Analytics.dart
+++ lib/Analytics.dart
@@ -1,6 +1,9 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:rempro/contant/theme.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class AnalyticsPage extends StatefulWidget {
   const AnalyticsPage({super.key});
@@ -8,7 +11,10 @@ class AnalyticsPage extends StatefulWidget {
   @override
   State<AnalyticsPage> createState() => _AnalyticsPageState();
 }
-
+String ku = '15-20';
+List<String> parts = ku.split('-');
+Double uk1 = double.parse(parts[0]) as Double;
+Double uk2 = double.parse(parts[1]) as Double;
 
 class _AnalyticsPageState extends State<AnalyticsPage> {
   @override
@@ -72,12 +78,88 @@ class _AnalyticsPageState extends State<AnalyticsPage> {
                   Divider(),
                   Gap(12.0),
                   Container(
-
+                    child: BarChartSample1(),
                   ),
                 ]
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class BarChartSample1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.5,
+      child: BarChart(
+        BarChartData(
+          //maxY: 20,
+          barTouchData: BarTouchData(
+            enabled: false,
+          ),
+          titlesData: const FlTitlesData(
+            show: true, // Show titles
+            bottomTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ) ,
+            leftTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+            rightTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+            topTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ), // Show bottom titles// Show left titles
+          ),
+          borderData: FlBorderData(
+              show: false
+          ),
+          backgroundColor: Colors.transparent,
+          gridData: FlGridData(show: false),
+
+          barGroups: [
+            BarChartGroupData(
+              x: 22,
+              barRods: [
+                BarChartRodData(toY: 18, color: Colors.blue, width: 20, // Change the width of the bar
+                  borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10),),)
+              ],
+            ),
+            BarChartGroupData(
+              x: 30,
+              barRods: [
+                BarChartRodData(toY: 12, color: Colors.blue,width: 20, // Change the width of the bar
+                  borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10),),),
+              ],
+            ),
+            BarChartGroupData(
+              x: 2,
+              barRods: [
+                BarChartRodData(toY: 15, color: Colors.blue,width: 20, // Change the width of the bar
+                  borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10),),)
+              ],
+            ),
+            BarChartGroupData(
+              x: 3,
+              barRods: [
+                BarChartRodData(toY: 10, color: Colors.grey.shade200,width: 20, // Change the width of the bar
+                  borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10),),)
+              ],
+            ),
+            BarChartGroupData(
+              x: 4,
+              barRods: [
+                BarChartRodData(toY: 8, color: Colors.grey.shade200,width: 20, // Change the width of the bar
+                  borderRadius: const BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10),),)
+              ],
+            ),
+          ],
+          //maxY: 20,
         ),
       ),
     );
